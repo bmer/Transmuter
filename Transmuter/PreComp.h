5568bc3e3460f7190da13ce066a2f475350edf6d
George Moromisato <public@neurohack.com>
2015-07-16 14:00:49 -0700

    Compile fixes

--- Transmuter/PreComp.h
+++ Transmuter/PreComp.h
@@ -4,7 +4,7 @@
 
 #ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
 #define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
+#endif
 
 #include <windows.h>
 #include <ddraw.h>
